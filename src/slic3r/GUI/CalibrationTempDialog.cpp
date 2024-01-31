535f7b3ef68c3f45b1f5d600c0b0d5c6dc6a033e
wschadow <wolfgang.schadow@t-online.de>
2024-01-31 19:50:03 +0100

    corretcs temp change gcode

--- src/slic3r/GUI/CalibrationTempDialog.cpp
+++ src/slic3r/GUI/CalibrationTempDialog.cpp
@@ -64,6 +64,7 @@ void CalibrationTempDialog::create_geometry(wxCommandEvent& event_args) {
     Model& model = plat->model();
     plat->new_project();
 
+
     //GLCanvas3D::set_warning_freeze(true);
     std::vector<size_t> objs_idx = plat->load_files(std::vector<std::string>{
             (boost::filesystem::path(Slic3r::resources_dir()) / "calibration" / "filament_temp" / "TempTowerBase.3mf").string()}, true, false, false);
@@ -90,94 +91,69 @@ void CalibrationTempDialog::create_geometry(wxCommandEvent& event_args) {
     long step_temp = 5;
     steps->GetValue().ToLong(&step_temp);
 
-    std::cout << "temps: " << temp_low << "  " << temp_high << "  " << step_temp << std::endl;
+     /// --- scale ---
+    double layer_height;
+    const ConfigOptionFloatOrPercent* first_layer_height_setting = printConfig->option<ConfigOptionFloatOrPercent>("first_layer_height");
+    double first_layer_height = first_layer_height_setting->get_abs_value(layer_height);
 
-    //size_t nb_items = 1 + nb_items_up + nb_items_down;
-    //start at the highest temp
-    //temperature = temperature + step_temp * nb_items_up;
+    const ConfigOptionFloat* layer_height_setting = printConfig->option<ConfigOptionFloat>("layer_height");
+    if (layer_height_setting != nullptr)
+        layer_height = layer_height_setting->value;
 
-    /// --- scale ---
-    //model is created for a 0.4 nozzle, scale xy with nozzle size.
-    const ConfigOptionFloats* nozzle_diameter_config = printerConfig->option<ConfigOptionFloats>("nozzle_diameter");
-    assert(nozzle_diameter_config->values.size() > 0);
-    float nozzle_diameter = nozzle_diameter_config->values[0];
-    float xyzScale = nozzle_diameter / 0.4;
+    double baseheight = 1.4;
+    int baselayers = std::round((1.4 - first_layer_height) / layer_height + 1);
+
+    double zScale = (first_layer_height + (baselayers-1) * layer_height) / baseheight;
+    double baseheightscaled  = baseheight * zScale;
+
     //do scaling
-    if (xyzScale < 0.9 || 1.1 < xyzScale) {
-        model.objects[objs_idx[0]]->scale(1.0, 1.0, xyzScale);
-    } else {
-        xyzScale = 1;
-        model.objects[objs_idx[0]]->scale(1.0, 1.0, xyzScale);
-    }
-
-    int16_t temp1 = temp_low;
-    int16_t temp2 = temp_high;
-
+    model.objects[objs_idx[0]]->scale(1.0, 1.0, zScale);
 
     // add other objects
+    double partheight = 10.0;
+    int partlayers = std::round(partheight / layer_height );
+    double zScale2 = partlayers * layer_height / partheight;
+    double partheightscaled = partheight * zScale2;
 
-    float zshift = 1.4 * xyzScale + 4.3;
-    std::cout << "zshift " << zshift << std::endl;
-
-    while (temp2 >= temp1){
-        add_part(model.objects[objs_idx[0]], (boost::filesystem::path(Slic3r::resources_dir()) / "calibration" / "filament_temp" / (std::to_string(temp2) + ".3mf")).string(),
-                Vec3d{ 0.0,0.0,zshift }, Vec3d{ 1.0,1.0,1.0 });
-        temp2 = temp2 - step_temp;
-        zshift = zshift + 10;
+    float zshift = 1.4 * zScale + 4.3;
+    for (int16_t temp = temp_high; temp >= temp_low; temp-= step_temp){
+        add_part(model.objects[objs_idx[0]], (boost::filesystem::path(Slic3r::resources_dir()) / "calibration" / "filament_temp" / (std::to_string(temp) + ".3mf")).string(),
+                Vec3d{ 0.0,0.0,zshift }, Vec3d{ 1.0,1.0,zScale2 });
+        zshift += 10;
     }
-
-
 
     /// --- main config, please modify object config when possible ---
     DynamicPrintConfig new_printConfig = *printConfig; //make a copy
     new_printConfig.set_key_value("complete_objects", new ConfigOptionBool(false));
 
-/*
     /// -- generate the heat change gcode
-    //std::string str_layer_gcode = "{if layer_num > 0 and layer_z  <= " + std::to_string(2 * xyzScale) + "}\nM104 S" + std::to_string(temperature - (int8_t)nb_delta * 5);
-    //    double print_z, std::string gcode,int extruder, std::string color
-    double firstChangeHeight = printConfig->get_abs_value("first_layer_height", nozzle_diameter);
-    //model.custom_gcode_per_print_z.gcodes.emplace_back(CustomGCode::Item{ firstChangeHeight + nozzle_diameter/2, CustomGCode::Type::Custom, -1, "", "M104 S" + std::to_string(temperature) + " ; ground floor temp tower set" });
-    model.objects[objs_idx[0]]->config.set_key_value("print_temperature", new ConfigOptionInt(temperature));
-    for (int16_t i = 1; i < nb_items; i++) {
-        model.custom_gcode_per_print_z.gcodes.emplace_back(CustomGCode::Item{ (i * 10 * xyzScale), CustomGCode::Type::Custom , -1, "", "M104 S" + std::to_string(temperature - i * step_temp) + " ; floor " + std::to_string(i) + " of the temp tower set" });
-        //str_layer_gcode += "\n{ elsif layer_z >= " + std::to_string(i * 10 * xyzScale) + " and layer_z <= " + std::to_string((1 + i * 10) * xyzScale) + " }\nM104 S" + std::to_string(temperature - (int8_t)nb_delta * 5 + i * 5);
-    }
-    //str_layer_gcode += "\n{endif}\n";
-    //DynamicPrintConfig new_printer_config = *printerConfig; //make a copy
-    //new_printer_config.set_key_value("layer_gcode", new ConfigOptionString(str_layer_gcode));
-*/
-    /// --- custom config ---
- /*   float brim_width = printConfig->option<ConfigOptionFloat>("brim_width")->value;
-    if (brim_width < nozzle_diameter * 8) {
-        model.objects[objs_idx[0]]->config.set_key_value("brim_width", new ConfigOptionFloat(nozzle_diameter * 8));
-    }
-    model.objects[objs_idx[0]]->config.set_key_value("brim_ears", new ConfigOptionBool(false));
-    model.objects[objs_idx[0]]->config.set_key_value("perimeters", new ConfigOptionInt(1));
-    model.objects[objs_idx[0]]->config.set_key_value("extra_perimeters_overhangs", new ConfigOptionBool(true));
-    model.objects[objs_idx[0]]->config.set_key_value("bottom_solid_layers", new ConfigOptionInt(2));
-    model.objects[objs_idx[0]]->config.set_key_value("top_solid_layers", new ConfigOptionInt(3));
-    model.objects[objs_idx[0]]->config.set_key_value("gap_fill_enabled", new ConfigOptionBool(false));
-    model.objects[objs_idx[0]]->config.set_key_value("thin_perimeters", new ConfigOptionPercent(100));
-    model.objects[objs_idx[0]]->config.set_key_value("layer_height", new ConfigOptionFloat(nozzle_diameter / 2));
-    model.objects[objs_idx[0]]->config.set_key_value("fill_density", new ConfigOptionPercent(7));
-//    model.objects[objs_idx[0]]->config.set_key_value("solid_fill_pattern", new ConfigOptionEnum<InfillPattern>(ipRectilinearWGapFill));
-//    model.objects[objs_idx[0]]->config.set_key_value("top_fill_pattern", new ConfigOptionEnum<InfillPattern>(ipRectilinearWGapFill));
-    //disable ironing post-process, it only slow down things
-    model.objects[objs_idx[0]]->config.set_key_value("ironing", new ConfigOptionBool(false));
+    int temperature = temp_high - step_temp;
+    int parts = std::round((temp_high - temp_low) / step_temp) + 1;
 
-*/
+    for (int16_t i = 1; i < parts; i++) {
+        double changelayer = partheightscaled * i + baseheightscaled + layer_height;
+        model.custom_gcode_per_print_z.gcodes.emplace_back(CustomGCode::Item{ changelayer, CustomGCode::Type::Custom , -1, "", "M104 S" + std::to_string(temperature) + " ; floor " + std::to_string(i+1) + " of the temp tower" });
+        temperature -= step_temp;
+    }
+
+    /// --- custom config ---
+    model.objects[objs_idx[0]]->config.set_key_value("brim_width", new ConfigOptionFloat(0.0));
+    model.objects[objs_idx[0]]->config.set_key_value("perimeters", new ConfigOptionInt(2));
+    model.objects[objs_idx[0]]->config.set_key_value("top_solid_layers", new ConfigOptionInt(3));
+    model.objects[objs_idx[0]]->config.set_key_value("bottom_solid_layers", new ConfigOptionInt(3));
+    model.objects[objs_idx[0]]->config.set_key_value("fill_density", new ConfigOptionPercent(7));
+    model.objects[objs_idx[0]]->config.set_key_value("fill_density", new ConfigOptionPercent(15));
+
+//   /// --- main config, please modify object config when possible ---
+    new_printConfig.set_key_value("skirts", new ConfigOptionInt(2));
+    new_printConfig.set_key_value("skirt_distance", new ConfigOptionFloat(1.0));
+
     //update plater
-    //GLCanvas3D::set_warning_freeze(false);
     this->gui_app->get_tab(Preset::TYPE_PRINT)->load_config(new_printConfig);
     plat->on_config_change(new_printConfig);
-    //this->gui_app->get_tab(Preset::TYPE_PRINTER)->load_config(new_printer_config);
-    //plat->on_config_change(new_printer_config);
     plat->changed_objects(objs_idx);
     this->gui_app->get_tab(Preset::TYPE_PRINT)->update_dirty();
-    //this->gui_app->get_tab(Preset::TYPE_PRINTER)->update_dirty();
     plat->is_preview_shown();
-    //update everything, easier to code.
     ObjectList* obj = this->gui_app->obj_list();
     obj->update_after_undo_redo();
 
